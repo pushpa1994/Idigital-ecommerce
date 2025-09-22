91fb0e12aafad456bf5ae891a7e8b589dff75778
Pushpa <pushpasanthi1994@gmail.com>
2025-09-22 14:19:16 +0530

    db connection

--- server/db/config.js
+++ server/db/config.js
@@ -1,5 +1,9 @@
 import mongoose from "mongoose";
 
-mongoose.connect(`mongodb://127.0.0.1:27017/idigital-ecommerce`).then((res)=>{
+// mongoose.connect(`mongodb://127.0.0.1:27017/idigital-ecommerce`).then((res)=>{
+//     console.log("db connected");
+// })
+mongoose.connect(`mongodb+srv://pushpasanthi1994_db_user:Push123@cluster0.kekovtt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/idigital-ecommerce`).then((res)=>{
     console.log("db connected");
 })
+
