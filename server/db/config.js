76fa8a0532d158b7f742568455a853c3e780cd67
Pushpa <pushpasanthi1994@gmail.com>
2025-09-22 14:30:37 +0530

    db fix

--- server/db/config.js
+++ server/db/config.js
@@ -3,7 +3,12 @@ import mongoose from "mongoose";
 // mongoose.connect(`mongodb://127.0.0.1:27017/idigital-ecommerce`).then((res)=>{
 //     console.log("db connected");
 // })
-mongoose.connect(`mongodb+srv://pushpasanthi1994_db_user:Push123@cluster0.kekovtt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/idigital-ecommerce`).then((res)=>{
+mongoose.connect(`mongodb+srv://pushpasanthi1994_db_user:Push123@cluster0.kekovtt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/idigital-ecommerce`,{
+useNewUrlParser: true,
+useUnifiedTopology: true
+}).then((res)=>{
     console.log("db connected");
 })
+
+
 
