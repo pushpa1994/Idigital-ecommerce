3eb2da53f90796c64ace7ca399c6660ae3954bb5
Pushpa <pushpasanthi1994@gmail.com>
2025-09-22 23:48:28 +0530

    dist change

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ app.use(express.static(path.join(__dirname, "../client/dist/client")));
 // });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/client/index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist/client/browser/index.html"));
 });
 
 
